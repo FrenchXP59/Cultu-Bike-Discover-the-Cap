93bfaa7d021c7745a2d9e8c5cf0a1af01f8ff30d
Frédéric LEGRAND <39912728-fredericlegrand@users.noreply.replit.com>
2025-04-16 21:15:40 +0000

    💡 Màj CultuBike v1.0 finalisée

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+// import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // ✅ Indispensable pour que Netlify serve correctement les assets
-
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/Images',
-          dest: 'assets'
-        }
-      ]
-    })
+    // viteStaticCopy({
+    //   targets: [
+    //     {
+    //       src: 'src/Images',
+    //       dest: 'assets'
+    //     }
+    //   ]
+    // })
   ],
   server: {
     host: '0.0.0.0',
